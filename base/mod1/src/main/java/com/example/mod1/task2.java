a877eae1f6f757d550ac455a8a24e200209ead37
Evgeniia Kordiukova <kordeugenie@yandex.ru>
2023-10-02 12:06:05 +0500

    base mod1 edit

--- base/mod1/src/main/java/com/example/mod1/task2.java
+++ base/mod1/src/main/java/com/example/mod1/task2.java
@@ -9,18 +9,16 @@ public class task2 {
                 {9, 12, 18},
                 {3, 4, 5}};
         int sum = 0;
-        int s = 0;
         int min = 1000;
         String res = "";
-        while (s < array.length) {
+        for (int[] ints : array) {
             for (int i = 0; i < array.length; i++) {
-                sum += array[s][i];
+                sum += ints[i];
             }
             if (sum < min) {
-                res = Arrays.toString(array[s]);
+                res = Arrays.toString(ints);
                 min = sum;
             }
-            s++;
             sum = 0;
         }
         System.out.println(res);
