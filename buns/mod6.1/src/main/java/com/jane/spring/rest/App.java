709524ca84ab276216965cbefc739073dd744156
Evgeniia Kordiukova <kordeugenie@yandex.ru>
2023-11-26 17:21:24 +0500

    buns/mod6.1 Client

--- buns/mod6.1/src/main/java/com/jane/spring/rest/App.java
+++ buns/mod6.1/src/main/java/com/jane/spring/rest/App.java
@@ -6,13 +6,8 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 
 import java.util.List;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args ) {
+public class App {
+    public static void main(String[] args) {
         AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(MyConfig.class);
         Communication communication = context.getBean("communication", Communication.class);
 
