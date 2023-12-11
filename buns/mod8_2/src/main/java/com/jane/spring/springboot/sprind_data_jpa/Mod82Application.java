0ee0904f66e21eed4757d6ce2a17246281cf7ff6
Evgeniia Kordiukova <kordeugenie@yandex.ru>
2023-12-11 15:17:22 +0500

    buns/mod8.2

---
+++ buns/mod8_2/src/main/java/com/jane/spring/springboot/sprind_data_jpa/Mod82Application.java
@@ -0,0 +1,13 @@
+package com.jane.spring.springboot.sprind_data_jpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Mod82Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Mod82Application.class, args);
+	}
+
+}
---
+++ buns/mod8_2/src/test/java/com/jane/spring/springboot/sprind_data_jpa/Mod82ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jane.spring.springboot.sprind_data_jpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Mod82ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
