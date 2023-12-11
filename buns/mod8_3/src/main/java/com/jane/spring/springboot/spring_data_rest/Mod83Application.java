314c5be5814319383b937a6cbf53c3a48158df7d
Evgeniia Kordiukova <kordeugenie@yandex.ru>
2023-12-11 15:44:09 +0500

    buns/mod8.3

---
+++ buns/mod8_3/src/main/java/com/jane/spring/springboot/spring_data_rest/Mod83Application.java
@@ -0,0 +1,13 @@
+package com.jane.spring.springboot.spring_data_rest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Mod83Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Mod83Application.class, args);
+	}
+
+}
