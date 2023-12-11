999c8c64258cc238212a2186a621ceabb353cd40
Evgeniia Kordiukova <kordeugenie@yandex.ru>
2023-12-11 14:39:24 +0500

    buns mod8.1

---
+++ buns/mod8/src/main/java/com/jane/spring/springboot/spring_course_springboot/Mod8Application.java
@@ -0,0 +1,13 @@
+package com.jane.spring.springboot.spring_course_springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Mod8Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Mod8Application.class, args);
+    }
+
+}
